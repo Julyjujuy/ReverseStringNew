17be3acb946842c4f5dbc3b7e925707d0a28a36d
Julyjujuy <tantissimo.entusiasmo@gmail.com>
2022-07-14 21:49:48 +0200

    Add project files.

---
+++ ReverseStringNew/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseStringNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str, Reversestr = "";
+            // storing string value
+
+            int Length;
+            //count lenght of string
+
+            Console.Write("Please Enter A String : ");
+            //show message 
+
+            Str = Console.ReadLine();
+            // allow user to input string
+
+            Length = Str.Length - 1;
+            //storing the length of given string
+
+            while (Length >= 0)
+            //loops the given string length
+            {
+                Reversestr = Reversestr + Str[Length];
+                //performimg a reverse string according to length of given string
+                Length--;
+            }
+            Console.WriteLine("Reverse  String  Is:  {0}", Reversestr); // displaying output to user
+            Console.ReadLine();
+
+        }
+    }
+}
